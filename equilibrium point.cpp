c2013055bc85377080378cf091faf8c737865d33
Gaurav kumar <102864247+yashswag22@users.noreply.github.com>
2023-07-31 10:01:51 +0530

    Create equilibrium point.cpp
    #equilibrium #point #problem #solution #O(n) #gfg #dsa #prefixsum

---
+++ equilibrium point.cpp
@@ -0,0 +1,22 @@
+class Solution{
+    public:
+    // Function to find equilibrium point in the array.
+    // a: input array
+    // n: size of array
+    int equilibriumPoint(long long a[], int n) {
+        if(n==1)
+        return 1;
+       for(int i=1;i<n;i++)
+       {
+           a[i]+=a[i-1];
+       }
+       for(int i=1;i<n;i++)
+       {
+           if(a[i]+a[i-1]== a[n-1])
+           return (i+1);
+       }
+       
+       return -1;
+    }
+
+};
